7eb1e5f1170f8e5898f9695fabdfdc8cb808c914
R. M. Mauney <rmmauney@gmail.com>
2018-07-16 23:47:08 -0400

    added images

---
+++ page-home.php
@@ -0,0 +1,36 @@
+<?php
+
+get_header();
+
+?>
+
+<div class="home-page-links">
+
+    <div class="home-link-container">
+        <img src="<?php echo get_stylesheet_directory_uri()?>/images/portfolio.jpg">
+        <a href="#"><h3>Portfolio</h3></a>
+    </div>
+
+    <div class="home-link-container">
+            <h2>John and Kate</h2>
+    </div>
+
+    <div class="home-link-container">
+            <p>Studio</p>
+    </div>
+
+    <div class="home-link-container">
+            Collections
+    </div>
+
+    <div class="home-link-container">
+            Contact
+    </div>
+
+</div><!-- /home-page-links -->
+
+<?php
+
+get_footer();
+
+?>
